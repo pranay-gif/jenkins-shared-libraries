44192bd53cffc410815a82a4e1ea2e531e7a60fd
Pranay Garai <72747143+pranay-gif@users.noreply.github.com>
2026-03-10 21:53:17 +0530

    Increase timeout duration for quality gate check

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,6 @@
 def call(){
-  timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+  
+  timeout(time: 10, unit: 'MINUTES') {
+    waitForQualityGate abortPipeline: true
+}
 }
